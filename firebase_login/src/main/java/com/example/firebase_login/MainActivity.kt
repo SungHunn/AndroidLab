95fa9367b7b286679f6d74b7aba46f4bb862d947
LAPTOP-P3BPH6JG\yoop1 <yoop1000@naver.com>
2023-07-23 18:03:02 +0900

    fierbase login 구현

---
+++ firebase_login/src/main/java/com/example/firebase_login/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.firebase_login
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.firebase_login.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        startActivity(Intent(this, AuthActivity::class.java))
+        return super.onOptionsItemSelected(item)
+    }
+
+
+}
